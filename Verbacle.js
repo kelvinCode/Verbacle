3c6adedcb2a04aceeadc8858794696f6150a35c6
Kelvin Cayari <kelvin.lorenzo.cayari@gmail.com>
2022-02-04 17:10:50 -0500

    put object array allQuestions into a variable

--- Verbacle.js
+++ Verbacle.js
@@ -1,15 +1,25 @@
-let allQuestions = [
-    {id: "1", question: "Legs are to humans as wheels are to ...", 
-        correctAnswer: "cars"},
-    {id: "2", question: "We ... things up by having a small meal.", 
-        answers: {a: "Wrapped", b: "Rapped", c: "Both are correct"},
-        correctAnswer: "a"},
-    {id: "3", question: "All he's good for is splitting ...", 
-        answers: {a: "Hares", b: "Hairs", c: "Both are correct"},
-        correctAnswer: "b"},
-    {id: "4", question: "Something that is entirely made of organs is called a(n)", 
-        correctAnswer: "organism"},
-]
+let verbacleQuiz = {
+    allQuestions: [
+        {Q1: {question: "Legs are to humans as wheels are to ...",
+            answers: {a: "cars"}, 
+            correctAnswer: "a"}
+            }, //this kind of question involves an input box
+        {Q2: {question: "We ... things up by having a small meal.", 
+            answers: {a: "Wrapped", b: "Rapped", c: "Both are correct"},
+            correctAnswer: "a"}
+            },
+        {Q3: {question: "All he's good for is splitting ...", 
+            answers: {a: "Hares", b: "Hairs", c: "Both are correct"},
+            correctAnswer: "b"}
+            },
+        {Q4: {question: "Something that is entirely made of organs is called a(n)",
+            answers: {a: "organism"}, 
+            correctAnswer: "a"}
+            }
+        ],
+    timer: 10,
+    maxScore: 4
+}
 
 let easyButtonInJS = document.getElementById("easyModeButton")
     easyButtonInJS.textContent = "EASY MODE"
@@ -23,6 +33,6 @@ let hardButtonInJS = document.getElementById("hardModeButton")
 
 function putEasyQuizOnPage() {
     let easyQuizInJS = document.getElementById("spaceForQuiz")
-        easyQuizInJS.textContent = allQuestions[1].question
-        easyQuizInJS.style.fontFamily = "Corbel"
+        easyQuizInJS.textContent = 
+            JSON.stringify(verbacleQuiz.allQuestions[Math.floor(Math.random() * verbacleQuiz.allQuestions.length)])
 }
