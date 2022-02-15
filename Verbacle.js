448eb80580e1676030c3fb3efaaef0b32aabe18f
Kelvin Cayari <kelvin.lorenzo.cayari@gmail.com>
2022-02-14 20:11:52 -0500

    made input questions have multiple correct answers and allowed start menu to fade out to make space for the quiz

--- Verbacle.js
+++ Verbacle.js
@@ -39,9 +39,10 @@ let allQuestions = [
     }
 ]
 
+let introPage = document.getElementById("introPage")
 let easyModeButton = document.getElementById("easyModeButton")
 easyModeButton.textContent = "EASY MODE"
-easyModeButton.addEventListener("click", giveQuestion)
+easyModeButton.addEventListener("click", clearStartPage)
 let mediumModeButton = document.getElementById("mediumModeButton")
 mediumModeButton.textContent = "MEDIUM MODE"
 mediumModeButton.addEventListener("click", giveQuestion)
@@ -49,33 +50,34 @@ let hardModeButton = document.getElementById("hardModeButton")
 hardModeButton.textContent = "HARD MODE"
 hardModeButton.addEventListener("click", giveQuestion)
 
+let quizPage = document.getElementById("quizPage")
 let questionBox = document.getElementById("questionBox")
 let questionText = document.getElementById("questionText")
 let answerContainer = document.getElementById("answerContainer")
 let timerContainer = document.getElementById("timerContainer")
 let revealContainer = document.getElementById("revealContainer")
 let quizOptionsDiv = document.getElementById("quizOptionsDiv")
-let quizResultsContainer = document.getElementById("quizResultsContainer")
+let resultsPage = document.getElementById("resultsPage")
 
 let currentQuestion = 0
 let correctAnswerCount = 0
 let generatedQuiz = allQuestions.sort(() => Math.random() - 0.5)
+
+function clearStartPage() {
+    easyModeButton.removeEventListener("click", clearStartPage)
+    introPage.style.transitionDuration = "0.3s"
+    introPage.style.opacity = 0
+    setTimeout(nextPage, 1000)
+    function nextPage() {
+        introPage.remove()
+        giveQuestion()
+    }
+}
         
 function giveQuestion() {
-    easyModeButton.removeEventListener("click", giveQuestion)
-    if (currentQuestion >= generatedQuiz.length) {
-        questionBox.remove()
-        let congratsMessage = document.createElement("p")
-        congratsMessage.textContent = "congrats bro you made the function work"
-        congratsMessage.style.fontFamily = "Corbel"
-        congratsMessage.style.textAlign = "center"
-        let totalScore = document.createElement("p")
-        totalScore.textContent = correctAnswerCount + "/" + generatedQuiz.length
-        totalScore.style.fontFamily = "Corbel"
-        totalScore.style.textAlign = "center"
-        quizResultsContainer.appendChild(congratsMessage)
-        quizResultsContainer.appendChild(totalScore)
-    } else {
+    quizPage.style.transitionDuration = "0.5s"
+    quizPage.style.opacity = 1
+    if (currentQuestion < generatedQuiz.length) {
         let nextButton = document.createElement("button")
         nextButton.textContent = "Next"
         nextButton.style.fontFamily = "Corbel"
@@ -101,17 +103,19 @@ function giveQuestion() {
         if (generatedQuiz[currentQuestion].isInput) {
             let answerInput = document.createElement("input")
             answerContainer.appendChild(answerInput)
-                answerInput.addEventListener("keypress", inputEntryResponse)
+            answerInput.addEventListener("keypress", inputEntryResponse)
                 function inputEntryResponse(keyboard) {
                 if (keyboard.key === "Enter") {
-                    generatedQuiz[currentQuestion].answers.find(checkAnswerText)
+                    let inputAnswerChecker = 0
+                    generatedQuiz[currentQuestion].answers.forEach(checkAnswerText)
                     function checkAnswerText(thisAnswer) {
+                        inputAnswerChecker++
                         if (answerInput.value === thisAnswer.text) {
                             console.log("You correct, homie")
                             clearInterval(questionCountdown)
                             correctAnswerCount++
                             nextButton.addEventListener("click", showNextQuestion)
-                        } else {
+                        } else if (inputAnswerChecker >= generatedQuiz[currentQuestion].answers.length) {
                             console.log("rong")
                             clearInterval(questionCountdown)
                             nextButton.addEventListener("click", showNextQuestion)
@@ -153,18 +157,34 @@ function giveQuestion() {
             currentQuestion++
             giveQuestion()
         }
+    } else if (currentQuestion >= generatedQuiz.length) {
+        questionBox.remove()
+        let congratsMessage = document.createElement("p")
+        congratsMessage.textContent = "congrats bro you made the function work"
+        congratsMessage.style.fontFamily = "Corbel"
+        congratsMessage.style.textAlign = "center"
+        let totalScore = document.createElement("p")
+        totalScore.textContent = correctAnswerCount + "/" + generatedQuiz.length
+        totalScore.style.fontFamily = "Corbel"
+        totalScore.style.textAlign = "center"
+        resultsPage.appendChild(congratsMessage)
+        resultsPage.appendChild(totalScore)
     }
 }
 
 // current features:
 // * every time the page is refreshed, the items in allQuestions are randomly shuffled.
 // * function "buttonClickResponse" checks to see if an input answer is correct
+// * function "checkAnswerText" uses a forEach method on the answers array, adds 1 to an  
+// index number after each iteration, and tells the user that they're right if their input 
+// matches one answer in the array, and wrong if the index is over the array length
 
 // observations:
 // * parameter for a function called in the "forEach" parentheses always refers to a 
 // single item in the array mentioned before the forEach.
-// * length of the array is NOT the same as the total amount of array items when counted, 
-// because when you count array items, you have to start at 0.
+// * because you have to start from 0 when counting array items using an index, the 
+// array.length ends up being +1 higher than the last number counted by the index, 
+// since the array.length assumes that the array items are being counted from 1. 
 // * "++" means you want the function to add 1 to the variable
 // * Time in Javascript is measured in milliseconds, so type "1000" if you want to put
 // a second in your code
@@ -173,4 +193,3 @@ function giveQuestion() {
 // * give the "restart" button its functionality
 // * function before "giveQuestion" that makes the intro text and difficulty
 // buttons disappear, then calls the "giveQuestion" function
-// * add conditional that checks if the input for answer input questions is correct
